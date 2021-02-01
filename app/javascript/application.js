3e33ee425f0f051fe98115d9ecacc11768db28b8
chepyego <jchepyego@gmail.com>
2021-02-02 00:37:52 +0300

    added jquery using yarn and mobile responsive nav

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,10 @@
 //= require materialize
+//require("@rails/ujs").start()
+//require("@turbolinks").start()
+//require("@rails/activestorage").start
+//require("channels")
+//require("jquery")
+
+$(document).ready(function(){
+      $('.sidenav').sidenav();
+})
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,14 @@
 const { environment } = require('@rails/webpacker')
+const env = require('@rails/webpacker/package/env')
+
+
+const webpack = require('webpack')
+environment.plugins.prepend('provide',
+  new webpack.ProvidePlugin({
+      $: 'jquery/src/jquery',
+      jQuery: 'jquery/src/jquery'
+  })
+
+)
 
 module.exports = environment
