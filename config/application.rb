50b315c87a3c34504ca897a2b924e96c5314e542
chepyego <jchepyego@gmail.com>
2021-02-03 17:45:30 +0300

    updated articles_index page

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,7 @@ module Blog
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.available_locales =[:en, :fr]
+    config.i18n.default_locale = :en
   end
 end
