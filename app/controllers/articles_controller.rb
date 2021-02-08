c4ec73e6d3c9e8454e3abb5570d1d25113f015c2
chepyego <jchepyego@gmail.com>
2021-02-09 02:19:10 +0300

    add devise for authentification

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i[ show edit update destroy ]
-   http_basic_authenticate_with name: "jemimah", password: "jemimah21", execept: [:index, :show]
-   http_basic_authenticate_with name: "jemimah", password: "jemimah21", only: :destroy
+  before_action :authenticate_user!, :except =>[:show, :index]
   # GET /articles or /articles.json
   def index
     @articles = Article.all
