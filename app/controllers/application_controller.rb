5a90bd068044842dc07fb35219f82bc6058b790d
chepyego <jchepyego@gmail.com>
2021-02-15 16:47:35 +0300

    added switch between locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,15 @@
 class ApplicationController < ActionController::Base
-   before_action :set_i18n_locale_from_params
+   before_action :set_locale
+   private
+   def set_locale
+    I18n.locale = extract_locale || I18n.default_locale
+   end
+   def extract_locale
+    parsed_locale = params[:locale]
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
+   end
 
-   protected
-        def set_i18n_locale_from_params
-            if params[:locale]
-                if I18n.available_locale.map(&:to_s).include?(params[:locale])
-                    I18n.locale = params[:locale]
-                else
-                    flash.now[:notice] = "#{params[:locale]} translation not available"
-                    logger.error flash.now[:notice]
-                end
-            end
-    end
-
-
+   def default_url_options
+    { locale: I18n.locale }
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  get 'articles/index'
-  root 'pages#home'
-  get 'pages/home'
-  get 'pages/Blog'
+  scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do    devise_for :users
+    get 'articles/index'
+    root 'pages#home'
+    get 'pages/home'
+    get 'pages/Blog'
 
-  # scope '(:locale)' do
+
     resources :articles do
       resources :comments
+    
+   end
+  end
 
   # end
-
-  end
   #  get 'articles/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
