67a9bf2062b14ce3f6b964813aa387ebc4c27951
chepyego <jchepyego@gmail.com>
2021-03-06 20:58:42 +0300

    modified spec user_log.rb

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe " user login ", type: :feature do
   let(:user) { create(:user) }
     scenario "with correct password and email" do
       visit new_user_session_path
-      fill_in 'user_email', with: Faker::Internet.email
-      fill_in 'user_password', with: Faker::Internet.password
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
       click_button 'Log in'
       expect(page).to have_content("Signed in successfully.")
         
