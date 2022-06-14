9649577c3f2a18761961a4f09e5a6c1a65d455cd
chepyego <jchepyego@gmail.com>
2022-06-15 02:22:35 +0300

    javascript click event

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,25 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+console.log('Hello from application.js')
+window.onload=function(){
+
+    const bodyContent = document.querySelector("#content1");
+    const newContent = document.querySelector("#author");
+    
+    const button1 = document.querySelector('#read-more');
+    
+    button1.addEventListener('click',()=>{
+        // newContent.innerHTML = "Jemimah chepyego";
+        if (bodyContent.className == "article"){
+            bodyContent.className = ""
+            button1.innerHTML = "show more";
+        } else{
+            newContent.innerHTML = " Author: Jemimah chepyego, from Kenya"
+
+            button1.innerHTML = "show less";
+        }
+    });  }
+
+
